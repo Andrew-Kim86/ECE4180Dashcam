d2e869230d65e02a9fdc0968090ad04ee55909a6
Andrew-Kim86 <47670816+Andrew-Kim86@users.noreply.github.com>
2021-04-30 00:33:56 -0400

    Working codegit add *git add *

--- main.cpp
+++ main.cpp
@@ -2,92 +2,90 @@
 #include "rtos.h"
 #include "uLCD_4DGL.h"
 #include "MMA8452.h"
-#include "XNucleo53L0A1.h"
+#include "VL53L0X.h"
 #include "speaker.h"
 #include <stdio.h>
 
-#define VL53L0_I2C_SDA   p28
-#define VL53L0_I2C_SCL   p27
+#define VideoLength 1
 
-uLCD_4DGL uLCD(p9,p10,p11);
 Serial pc(USBTX,USBRX);
-MMA8452 acc(p28, p27, 9600);
-Speaker mySpeaker(p21);
-static XNucleo53L0A1 *board=NULL;
+uLCD_4DGL uLCD(p9,p10,p11);
+MMA8452 acc(p28, p27, 40000);
+DevI2C i2c(p28, p27);
 DigitalOut shdn(p26);
-Mutex i2c;
+VL53L0X sensor(&i2c, &shdn, NC);
+DigitalIn power(p5);
 
+//Data
 double ax, ay, az;
-int status;
-uint32_t distance;
-
 int numOfPark;
 int numOfAcc;
 int numOfVideos;
-
+ 
 void display()
 {
-    i2c.lock();
-    uLCD.cls();
-    uLCD.printf("Parking Incidents:\n%d\n", numOfPark);
-    uLCD.printf("Traffic Incidents:\n%d\n", numOfAcc);
-    uLCD.printf("Number of Videos:\n%d\n", numOfVideos);
-    i2c.unlock();
+    uLCD.locate(0,1);
+    uLCD.printf("Parking Incidents:%d\n\n", numOfPark);
+    uLCD.printf("Traffic Incidents:%d\n\n", numOfAcc);
+    uLCD.printf("Number of Videos: %d\n\n", numOfVideos);
 }
-
-void pulse(void const *argument)
-{
-    while(1)
-    {
-        i2c.lock();
-        acc.readXYZGravity(&ax,&ay,&az);
-        i2c.unlock();
-        if(ax>1 || ay > 1)
-        {
-            mySpeaker.PlayNote(969.0, 0.5, 1.0);
-        }
-        pc.printf("x:%lf   y:%lf z:%lf\r\n",ax,ay,az);
-        Thread::wait(50);
-    }
-}
-
+ 
 int main() {
-    uLCD.cls();
     numOfPark = 0;
     numOfAcc = 0;
     numOfVideos = 0;
     display();
-    //acc.setDynamicRange(DYNAMIC_RANGE_8G);
-    int status;
+    
+    //Hardware
+    acc.setBitDepth(MMA8452::BIT_DEPTH_12);
+    acc.setDynamicRange(MMA8452::DYNAMIC_RANGE_4G);
+    acc.setDataRate(MMA8452::RATE_400);
+    sensor.VL53L0X_off();
+    sensor.init_sensor(0x30);
+    sensor.VL53L0X_on();
     uint32_t distance;
-    DevI2C *device_i2c = new DevI2C(VL53L0_I2C_SDA, VL53L0_I2C_SCL);
-    /* creates the 53L0A1 expansion board singleton obj */
-    board = XNucleo53L0A1::instance(device_i2c, A2, D8, D2);
-    shdn = 0; //must reset sensor for an mbed reset to work
-    wait(0.1);
-    shdn = 1;
-    wait(0.1);
-    /* init the 53L0A1 board with default values */
-    status = board->init_board();
-    while (status) {
-        //pc.printf("Failed to init board! \r\n");
-        status = board->init_board();
-    }
-    //Thread thread1(display);
-    //Thread thread2(pulse);
-    while (1) {
-        i2c.lock();
-        status = board->sensor_centre->get_distance(&distance);
-        i2c.unlock();
-        if (status == VL53L0X_ERROR_NONE) {
-            if(distance != 0 && distance < 100)
+    while(1) {
+        //Get data
+        int status = sensor.get_distance(&distance);
+        if(!acc.isXYZReady()) {
+            wait(0.01);
+            continue;
+        }
+        acc.readXYZGravity(&ax,&ay,&az);
+        //Process
+        if (power == 1)
+        {
+            if(abs(ax) >= 1.5 || abs(ay) >= 1.5 || abs(az-1) >= 1.5)
             {
-                pc.printf("D=%ld mm\r\n", distance);
-                numOfPark++;
-                //display();
-                wait(15);
+                pc.printf("AccTrigDrive %lf %lf %lf\n", ax, ay, az);
+                numOfAcc++;
+                numOfVideos++;
+                display();
+                wait(VideoLength);
             }
         }
-        Thread::wait(500);
+        else
+        {
+            if(abs(ax) >= 1 || abs(ay) >= 1)
+            {
+                pc.printf("AccTrigPark\n");
+                numOfPark++;
+                numOfVideos++;
+                display();
+                wait(VideoLength);
+            }
+            else
+            {
+                if (status == VL53L0X_ERROR_NONE && distance <= 100) 
+                {
+                    pc.printf("VL53L0X [mm]:            %6ld\r\n", distance);
+                    numOfPark++;
+                    numOfVideos++;
+                    display();
+                    wait(VideoLength);
+                }
+            } 
+        }
+        wait(0.1);
     }
 }
